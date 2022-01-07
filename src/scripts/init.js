6fca6a317c6b226622b072d7b201dac46f39edb6
Nikita <main.acr0matic@gmail.com>
2022-01-07 16:46:05 +0300

    feat: валидация форм

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -4,3 +4,13 @@ _.forEach(accordion, (item) => new handorgel(item));
 const scrollController = new SmoothScroll('a[href*="#"]', {
   speed: 300,
 });
+
+tippy('[data-tippy-content]', {
+  allowHTML: true,
+  maxWidth: 300,
+  theme: 'flat',
+  placement: 'left',
+});
+
+const forms = document.querySelectorAll('form');
+_.forEach(forms, form => new Form(form));
