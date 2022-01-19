e62f02a9fee050d21d38764cf9f6591e17e8bf4d
Nikita <main.acr0matic@gmail.com>
2022-01-20 01:48:49 +0300

    feat: верстка "аутстаффинг"

---
+++ src/outstaff.php
@@ -0,0 +1,209 @@
+<?php include_once('partials/header.php'); ?>
+
+<main>
+  <section style="background-image: url('img/page/outstaff/hero/background.svg');" id="hero" class="section section--primary">
+    <div class="container">
+      <div class="hero">
+        <div class="hero__content">
+          <h1 class="hero__title hero__title--big section__title page__title">
+            Аутстаффинг
+            <br>
+            персонала
+          </h1>
+
+          <h2 class="hero__subtitle section__subtitle page__subtitle">
+            Выведите персонал из штата
+            <br>
+            и избавьтесь от бесконечного
+            <br>
+            заполнения документов
+          </h2>
+
+          <div class="hero__action">
+            <button data-micromodal-trigger="modal-callback" class="button button-primary me-2">Подобрать персонал</button>
+            <button class="button button-outline">Вакансии</button>
+          </div>
+          <!-- /.hero__action -->
+        </div>
+        <!-- /.hero__content -->
+
+        <div class="hero__picture hero__picture--big">
+          <img class="hero__image image" src="img/page/outstaff/hero/illustration.png" alt="">
+        </div>
+        <!-- /.hero__picture -->
+      </div>
+      <!-- /.hero -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <section data-name="Подробнее" id="explain" class="section">
+    <div class="container">
+      <div class="section__header text-center">
+        <h2 class="section__title section__title--underline">Что такое аутстаффинг</h2>
+      </div>
+      <!-- /.section__header -->
+
+      <div class="explain">
+        <div class="row">
+          <div class="col-4">
+            <div class="explain__item">
+              <img src="img/page/outstaff/explain/coins.svg" alt="" class="explain__icon image">
+              <h3 class="explain__title">
+                Индивидуальный
+                <br>
+                менеджер
+              </h3>
+              <p class="explain__description">
+                Вашим заказом будет заниматься
+                <br>
+                один менеджер - не будет суеты
+                <br>
+                и неразберихи в процессе работы
+              </p>
+            </div>
+            <!-- /.explain__item -->
+          </div>
+          <!-- /.col-4 -->
+
+          <div class="col-4">
+            <div class="explain__item">
+              <img src="img/page/outstaff/explain/case.svg" alt="" class="explain__icon image">
+              <h3 class="explain__title">
+                Быстрая замена
+                <br>
+                сотрудника
+              </h3>
+              <p class="explain__description">
+                В течение 2 часов. Бесплатная
+                <br>
+                замена сотрудника в течение
+                <br>
+                гарантийного срока
+              </p>
+            </div>
+            <!-- /.explain__item -->
+          </div>
+          <!-- /.col-4 -->
+
+          <div class="col-4">
+            <div class="explain__item">
+              <img src="img/page/outstaff/explain/graph.svg" alt="" class="explain__icon image">
+              <h3 class="explain__title">
+                Работаем без
+                <br>
+                выходных
+              </h3>
+              <p class="explain__description">
+                Работа не приостановится
+                <br>
+                в самый важный момент - рабочий
+                <br>
+                план будет выполнен в срок
+              </p>
+            </div>
+            <!-- /.explain__item -->
+          </div>
+          <!-- /.col-4 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.advantaexplainge -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <section data-name="Задачи" id="textblock" class="section">
+    <div class="container">
+      <div class="textblock">
+        <div class="section__header">
+          <h2 class="section__title section__title--underline">
+            Почему стоит обратиться
+            <br>
+            за аутстаффингом
+          </h2>
+        </div>
+        <!-- /.section__header -->
+
+        <div class="row flex-row-reverse">
+          <div class="col-5 offset-1 position-relative">
+            <div class="textblock__picture mt-0">
+              <img src="img/page/outstaff/textblock/picture.png" alt="" class="textblock__image image">
+            </div>
+            <!-- /.textblock__picture -->
+          </div>
+          <!-- /.col-5 -->
+
+          <div class="col-6">
+            <div class="textblock__content">
+              <ul class="textblock__list list list--big">
+                <li class="list__item">
+                  Законодательство РФ обновляется ежегодно, Трудовая вносит корректировки. Вы можете не успеть адаптироваться и получить штрафы. Наши юристы проверяют актуальность сводов закона несколько раз в месяц
+                </li>
+
+                <li class="list__item">
+                  Штраф за нарушение миграционного законодательства. Ошибка вашего кадрового специалиста обойдется вам слишком дорого
+                </li>
+
+                <li class="list__item">
+                  Мы знаем как по статье из Налогового Кодекса уменьшить уплату налогов на 37%, что значительно повлияет на вашу бухгалтерию
+                </li>
+              </ul>
+            </div>
+            <!-- /.textblock__content -->
+          </div>
+          <!-- /.col-6 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.textblock -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <section data-name="Оставить заявку" id="textblock" class="section section--last">
+    <div class="container">
+      <div class="textblock">
+        <div class="row align-items-center">
+          <div class="col-5 position-relative">
+            <div class="textblock__picture mt-0">
+              <img src="img/page/outstaff/contact/picture.png" alt="" class="textblock__image image">
+            </div>
+            <!-- /.textblock__picture -->
+          </div>
+          <!-- /.col-5 -->
+
+          <div class="col-6 offset-1 ">
+            <div class="textblock__content">
+              <div class="section__header">
+                <h2 class="section__title section__title--underline">
+                  Оставьте заявку
+                  <br>
+                  на подбор персонала
+                </h2>
+              </div>
+              <!-- /.section__header -->
+
+              <p class="textblock__paragraph">
+                Ответьте на пару вопросов и определите примерную стоимость найма персонала
+                в вашу фирму
+              </p>
+
+              <div class="textblock__action">
+                <button class="textblock__button button button-primary">Подобрать персонал</button>
+              </div>
+              <!-- /.textblock__action -->
+            </div>
+            <!-- /.textblock__content -->
+          </div>
+          <!-- /.col-6 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.textblock -->
+    </div>
+    <!-- /.container -->
+  </section>
+</main>
+
+<?php include_once('partials/footer.php'); ?>
